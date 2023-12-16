8a0f874970b7b2d77969dc174ccd0caaff64c450
Murad Najafli <muradnecefli05@gmail.com>
2023-12-16 14:13:41 +0400

    - Initial Commit

--- app/src/main/java/com/muradnajafli/carfactsapp/ui/FactsViewModel.kt
+++ app/src/main/java/com/muradnajafli/carfactsapp/ui/FactsViewModel.kt
@@ -5,13 +5,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.muradnajafli.carfactsapp.data.utils.Car
-import dagger.assisted.Assisted
-import dagger.assisted.AssistedInject
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 
-
-class FactsViewModel(private val selectedCar: Car) : ViewModel() {
+class FactsViewModel(selectedCar: Car) : ViewModel() {
 
     var fact by mutableStateOf(generateRandomFact(selectedCar))
 
